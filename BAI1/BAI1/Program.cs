0960811b678c427cb00479d02709262669331a49
HoangAnh <nguyehoanganh8976@gmail.com>
2022-03-12 02:14:41 +0700

    BT OPP

---
+++ BAI1/BAI1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bai1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Coodinator coodinator = new Coodinator();
+            coodinator.setX(1);
+            coodinator.setY(4);
+            coodinator.setName("X");
+
+            //Lấy các giá trị từ đối tượng coodinator
+            Console.WriteLine(coodinator.getName());
+            Console.WriteLine(coodinator.getX());
+            Console.WriteLine(coodinator.getY());
+
+            //Gán giá trị cho đối tượng lúc new
+            Coodinator coodinator1 = new Coodinator();
+            Coodinator coodinator2 = new Coodinator(2, 2, "Y");
+
+            Console.WriteLine(coodinator2);
+
+
+
+            Console.WriteLine();
+        }
+    }
+}
